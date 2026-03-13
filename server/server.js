6916a9afe62a1e4df7555982545075e796892502
Utkarsh Pushpankar <upushpankar@gmail.com>
2026-03-13 08:53:03 +0530

    fix: vercel serverless express setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
 require('express-async-errors');
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
